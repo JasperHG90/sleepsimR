dfe2114502114613776ba1129545addc4b84330f
Jasper Ginn <jasperginn@gmail.com>
2020-02-26 17:32:01 +0100

    Added class methods to remove burn-in samples

--- R/utils.R
+++ R/utils.R
@@ -27,3 +27,39 @@ intercepts_to_TPM <- function(coefs) {
   # Return
   return(tpm_prob)
 }
+
+# S3 generic
+burn <- function(x, ...) {
+  UseMethod("burn", x)
+}
+# Burn function for model output
+burn.mHMM_cont <- function(x) {
+  # Number of burn_in samples
+  burn_in <- x$input$burn_in
+  J <- x$input$J
+  # For each element (and nested elements), remove the burn-in samples
+  inp <- x$input
+  x$input <- NULL
+  # Get data types for each
+  dtypes <- vapply(x, function(x) mode(x), "string")
+  for(idx in seq_along(x)) {
+    # If character, pass
+    if(mode(x[[idx]]) == "character") {
+      next
+    } else if (mode(x[[idx]]) == "list") {
+      for(subj_idx in seq_along(x[[idx]])) {
+        x[[idx]][[subj_idx]] <- x[[idx]][[subj_idx]][(burn_in+1):J,]
+      }
+    } else {
+      if (nrow(x[[idx]] < J)) {
+        next
+      }
+      x[[idx]] <- x[[idx]][(burn_in+1:J),]
+    }
+  }
+  # Create new object and return
+  x$input <- inp
+  class(x) <- "mHMM_cont"
+  # Return
+  return(x)
+}
