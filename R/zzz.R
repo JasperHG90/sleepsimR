25b689caaa4a2fbfa4658510640831c54b3940e0
Jasper Ginn <jasperginn@gmail.com>
2020-02-26 16:38:16 +0100

    Add default simulation options

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,18 @@
 ## Load on startup
 
 .onLoad <- function(libname = find.package("sleepsimR"), pkgname="sleepsimR") {
-  # Assign unique id
-  if(Sys.getenv("SLEEPSIMR_UID") == "") {
-    Sys.setenv("SLEEPSIMR_UID" = uuid::UUIDgenerate())
-  }
   # Set API host is not already in environment
   if(Sys.getenv("SLEEPSIMR_MASTER_HOST" == "")) {
     Sys.setenv("SLEEPSIMR_MASTER_HOST" = "http://localhost:5007")
   }
+  # Hard-code options in the package
+  options(
+    sleepsimR_simulate = list(
+      "m" = 3,                # Sleep states
+      "distr" = "continuous", # Simulate a continuous distribution
+      "n_dep" = 2            # Number of dependent variables
+    ),
+    # Unique ID for this session
+    sleepsimR_uid = uid::UUIDgenerate()
+  )
 }
