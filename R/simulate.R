8f3e3ce4c5e0b5e947d01e00df1f2d54b32f4bf9
Jasper Ginn <jasperginn@gmail.com>
2020-02-26 16:38:41 +0100

    Working on simulation function

--- R/simulate.R
+++ R/simulate.R
@@ -1,1 +1,15 @@
 ## Functions used to simulate data are placed here
+
+#' Simulate a sleep dataset for use in an mHMM model
+#'
+#' @param n
+#' @param n_t
+#' @param zeta
+#' @param Q
+#'
+#' @return
+#'
+#' @details
+simulate_dataset <- function(n, n_t, zeta, Q) {
+
+}
