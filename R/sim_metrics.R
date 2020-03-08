f9880f3427e5ed191f0d4cb3751aa568de04698f
Jasper Ginn <jasperginn@gmail.com>
2020-03-08 10:13:34 +0100

    Add simulation metrics

--- R/sim_metrics.R
+++ R/sim_metrics.R
@@ -6,28 +6,46 @@
 #' @param true_param_value numeric. Value of the ground-truth parameter.
 #' @param simulated_param_values k-length numeric vector. k >= 1 and holds the parameter values of the estimated parameters.
 #'
-#' @return average bias of simulated values versus the ground-truth value
+#' @return numeric vector. Contains two elements: (1) average bias of simulated values versus the ground-truth value and (2) MCMC SE of the bias value
 #'
 #' @details This function computes the percentage bias by using the signed mean difference.
 #'
 #' @export
 bias <- function(true_param_value, simulated_param_values) {
-  mean(simulated_param_values - true_param_value)
+  bias <- mean(simulated_param_values - true_param_value)
+  bmcse <- bias_MCMC_SE(simulated_param_values)
+  ret <- c(bias, bmcse)
+  names(ret) <- c("bias", "MCMC_SE")
+  return(ret)
 }
 
 #' MCMC Standard Error of the bias value
 #'
-#' @param
+#' @param x k-length numeric vector. k >= 1 and holds the parameter values of the estimated parameters.
+#'
+#' @return Bias MCMC SE
+bias_MCMC_SE <- function(x) {
+  nsim <- length(x)
+  a <- (1 / (nsim - 1))
+  b <- sum((x - mean(x))^2)
+  return(
+    sqrt((a * b)/(nsim))
+  )
+}
 
 #' Compute the emperical SE
 #'
 #' @param x numeric vector. Simulated parameter estimates
 #'
-#' @return numeric scalar. Emperical standard error.
+#' @return numeric vector with two values: (1) Emperical standard error and (2) MCMC SE of this value
 #'
 #' @export
 emperical_SE <- function(x) {
-  sqrt((1/(length(x) - 1)) * sum((x - mean(x))^2))
+  ESE <- sqrt((1/(length(x) - 1)) * sum((x - mean(x))^2))
+  MCMCSE <- emperical_MCMC_SE(ESE, length(x))
+  ret <- c(ESE, MCMCSE)
+  names(ret) <- c("emperical_se", "MCMC_SE")
+  return(ret)
 }
 
 #' Compute the MCMC SE
@@ -35,10 +53,8 @@ emperical_SE <- function(x) {
 #' @param x numeric vector. Simulated parameter estimates.
 #'
 #' @return numeric scalar. MCMC standard error.
-#'
-#' @export
-MCMC_SE <- function(x) {
-  emperical_SE(x) / sqrt(2*(length(x) - 1))
+emperical_MCMC_SE <- function(emp_se, nsim) {
+  emp_se / sqrt(2*nsim - 1)
 }
 
 #' Compute coverage
@@ -46,9 +62,23 @@ MCMC_SE <- function(x) {
 #' @param CI list. Each element contains the upper and lower values of the 95% CI of an interation.
 #' @param true_param_value numeric scalar. True value of the parameter.
 #'
-#' @return numeric scalar. Probability that the 95% CI contains the true value.
+#' @return numeric vector with two values: (1) Probability that the 95% CI contains the true value and (2) MCMC Standard Error
 #'
 #' @export
-coverage <- function(x) {
+coverage <- function(CI, true_param_value) {
+  cvr <- mean(vapply(CI, function(x) (x[1] <= true_param_value) & (true_param_value <= x[2]), 0))
+  cvrSE <- coverage_MCMC_SE(cvr, length(CI))
+  ret <- c(cvr, cvrSE)
+  names(ret) <- c("Coverage", "MCMC_SE")
+  return(ret)
+}
 
+#' Coverage MCMC Standard Error
+#'
+#' @param coverage
+#' @param nsim
+#'
+#' @return
+coverage_MCMC_SE <- function(coverage, nsim) {
+  sqrt((coverage * (1-coverage))/(nsim))
 }
